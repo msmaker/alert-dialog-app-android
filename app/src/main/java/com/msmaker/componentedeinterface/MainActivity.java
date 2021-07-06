83674bf25f378c9f6e1b7d072ab0082b5f4a8528
Marco Silva <marcosilva.tec@gamil.com>
2021-07-06 14:22:25 -0300

    Alert Dialog

---
+++ app/src/main/java/com/msmaker/componentedeinterface/MainActivity.java
@@ -0,0 +1,55 @@
+package com.msmaker.componentedeinterface;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirDialog(View view) {
+
+        //Instanciar AlertDialog
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        //Configurar titulo e mensagem
+        dialog.setTitle("Titulo da dialog");
+        dialog.setMessage("Mensagem da Dialog");
+
+        //Configurar acoes para sim e nao
+        dialog.setPositiveButton("sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Executar ação ao clicar no botão sim",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+        dialog.setNegativeButton("não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Executar ação ao clicar no botão não",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+        dialog.create();
+        dialog.show();
+    }
+}
